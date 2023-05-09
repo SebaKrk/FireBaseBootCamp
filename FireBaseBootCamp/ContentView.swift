4dce7f6418da9ded7ba0e2ffbd53ec049b240a64
SebaKrk <s.sciuba@gmail.com>
2023-05-09 09:52:44 +0200

    add Crashlytics

--- FireBaseBootCamp/ContentView.swift
+++ FireBaseBootCamp/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Button("Crash") {
+          fatalError("Crash was triggered")
+        }
+        
     }
 }
 
